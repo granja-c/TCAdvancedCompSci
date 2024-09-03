26b39731f4b4df3ff019c439d0883ef4a25bf6b8
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-09-03 12:16:50 -0500

    update

---
+++ src/Q1/LP510.java
@@ -0,0 +1,15 @@
+package Q1;
+import java.util.Scanner;
+
+public class LP510 {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        System.out.print("Number 1: ");
+        int n1 = sc.nextInt();
+        System.out.print("Number 2: ");
+        int n2 = sc.nextInt();
+        while (n2 > 0) {
+            if
+        }
+    }
+}
