162135234cd76d357d97f80288e608eff9c4b9bc
granja-c <granja.c@stu.janesville.k12.wi.us>
2024-09-04 16:30:16 -0500

    update

--- src/Q1/LP519.java
+++ src/Q1/LP519.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class LP519 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        String srch = sc.next();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.substring(i, i + srch.length()).equals(srch)) {
-
+        System.out.println("Sentence: ");
+        String str = sc.nextLine();
+        System.out.print("Word to search for: ");
+        String word = sc.next();
+        String res = "";
+        for (int i = 0; i < str.length()-word.length(); i++) {
+            if (str.substring(i, i + word.length()).equals(word)) {
+                res += str.substring(0, i) + str.substring(i+1+word.length());
             }
         }
+        System.out.println(res);
     }
 }
+/*
+Sentence:
+hello cat hi
+Word to search for: cat
+hello hi
+ */
