9a2fade00c0aa28a9a0b748ffd02581340e47c9e
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-09-06 12:17:14 -0500

    update

--- src/Q1/LP313.java
+++ src/Q1/LP313.java
@@ -3,7 +3,21 @@ import Utils.Console;
 
 public class LP313 {
     public static void main(String[] args) {
-        double princ = Console.input("principal: ");
-
+        double princ = Console.input("Principal: ");
+        int yrs = Console.input("Years: ");
+        double rate = Console.input("Interest Rate: ");
+        double amt = princ * (1 + yrs * rate);
+        System.out.printf("The value after the term: $%.2f\n", amt);
+        double des = Console.input("Desired amount: ");
+        double pr = des / (1 + yrs * rate);
+        System.out.printf("Principal: %.2f", pr);
     }
 }
+/*
+Principal: 5000.
+Years: 5
+Interest Rate: .06
+The value after the term: $6500.00
+Desired amount: 6500.
+Principal: 5000.00
+ */
---
+++ src/Q1/Prog505b.java
@@ -0,0 +1,20 @@
+package Q1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog505b {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("Langdat/prog505.txt"));
+            // String names =
+            while (sc.hasNext()) {
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file");
+        }
+    }
+}
