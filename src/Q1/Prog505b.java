5834c0de01319376c33321ac0850cfd858e54bb1
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-09-11 12:17:34 -0500

    update

--- src/Q1/Prog505b.java
+++ src/Q1/Prog505b.java
@@ -4,11 +4,34 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+class Student {
+    private String first;
+    private String last;
+    private int[] grades;
+    private double avg;
+    private String letter;
+
+    public Student(String fn, String ln, int[] gs) {
+        first = fn;
+        last = ln;
+        grades = gs;
+        avg = 0;
+    }
+    public void calcGrade() {
+        for (int i : grades) {
+            avg += i;
+        }
+        avg /= grades.length;
+    }
+    public String toString() {
+        return String.format(first + " " + last + "\t%d\t%d\t%d\t%d\t%d\t%d", );
+    }
+}
 public class Prog505b {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(new File("Langdat/prog505.txt"));
-            // String names =
+
             while (sc.hasNext()) {
 
             }
