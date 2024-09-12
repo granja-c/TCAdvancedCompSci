d1393de72d51fd7df0cbf3f318babdb3243cafb4
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-09-12 12:17:00 -0500

    update

--- src/Q1/Prog705x.java
+++ src/Q1/Prog705x.java
@@ -11,15 +11,16 @@ public class Prog705x {
             Scanner sc1 = new Scanner(new File("Langdat/prog512h.dat"));
             Scanner sc2 = new Scanner(new File("Langdat/prog705x.dat"));
             var poem = new ArrayList<String>();
-            var code = new ArrayList<Integer>();
+            var code = new ArrayList<String>();
             while (sc1.hasNext()) {
                 String line = sc1.nextLine();
                 poem.add(line);
             }
             while (sc2.hasNext()) {
-                int n = sc1.nextInt();
+                String n = sc1.nextLine();
                 code.add(n);
             }
+
 
         } catch (IOException e) {
             System.out.println("Can't find data file");
