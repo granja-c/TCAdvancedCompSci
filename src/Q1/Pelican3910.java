ec3841dfe6f1c0d28d26e70dda113337d66ef9b5
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-09-19 12:18:02 -0500

    update

---
+++ src/Q1/Pelican3910.java
@@ -0,0 +1,19 @@
+package Q1;
+import java.io.*;
+import java.util.*;
+
+class ModFib {
+    public static int modFibonacci(int n) {
+
+        return 0;
+    }
+}
+public class Pelican3910 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Generate which term number? ");
+        int k = sc.nextInt();
+
+        System.out.println("Term #" + k + " is " + ModFib.modFibonacci(k));
+    }
+}
---
+++ src/Q1/Pelican504.java
@@ -0,0 +1,11 @@
+package Q1;
+
+public class Pelican504 {
+    public static void main(String[] args) {
+        int i[] = {-7, 15, 21, 22, 43, 49, 51, 67, 78, 81, 84, 89, 95, 97};
+        Integer iw[] = new Integer[14];
+        for (int k = 0; k < 14; k++) {
+
+        }
+    }
+}
