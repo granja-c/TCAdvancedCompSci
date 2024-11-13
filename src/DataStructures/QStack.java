41475d3991563448109d865ee76cf03692056d04
granja.c <granja.c@stu.janesville.k12.wi.us>
2024-11-13 12:12:25 -0600

    update

--- src/DataStructures/QStack.java
+++ src/DataStructures/QStack.java
@@ -1,13 +1,14 @@
 package DataStructures;
 
 public class QStack<T extends Comparable<T>>{
-    private final Queue<T> stack;
+    private Queue<T> stack;
 
-    public QStack() { stack = new Queue<>(); }
+    public QStack() {
+        stack = new Queue<>();
+    }
 
-//    public void push(T element) { stack.x(element);}
+// public void push(T element) { stack.x(element);}
 //    public T pop() { return stack.x(); }
 //    public T peek() { return stack.x(); }
-//    public int size() { return stack.size; }
 //    public boolean isEmpty() { return stack.isEmpty(); }
 }
---
+++ src/Q2/Prog1071h/Prog1071h.java
@@ -0,0 +1,7 @@
+package Q2.Prog1071h;
+
+public class Prog1071h {
+    public static void main(String[] args) {
+
+    }
+}
