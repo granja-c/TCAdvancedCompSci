7891f7daedf2bd6f1f6d89919f0c6a0957b42689
granja.c <granja.c@stu.janesville.k12.wi.us>
2025-01-02 12:16:23 -0600

    update

--- src/Q2/MSOE247.java
+++ src/Q2/MSOE247.java
@@ -7,14 +7,19 @@ public class MSOE247 {
         double wid;
         double len;
         double hei;
+        double vol;
         public gift(String na, double wi, double le,double hi) {
             name = na;
             wid = wi;
             len = le;
             hei = hi;
+            vol = len * wid * hei;
         }
         public void print() {
             System.out.print(name + ": " + wid + "x" + len + "x" + hei);
+        }
+        public double getVol(){
+            return vol;
         }
     }
     public static void main(String[] args) {
@@ -25,15 +30,15 @@ public class MSOE247 {
         String w = "";
         String l = "";
         String h = "";
+        str = input.nextLine();
         while (!str.equals("ignore 0x0x0")) {
-            str = input.nextLine();
             int sp = 0;
             for (int i = 0; i < str.length(); i++) {
                 if (!str.substring(i, i+1).equals(" ")) {
                     n += str.substring(i, i+1);
                 }
                 if (str.substring(i, i+1).equals(" ")) {
-                    sp = i;
+                    sp = i+1;
                     i = str.length();
                 }
             }
@@ -44,7 +49,7 @@ public class MSOE247 {
                     w += str.substring(i, i+1);
                 }
                 if (str.substring(i, i+1).equals("x")) {
-                    sp2 = i;
+                    sp2 = i+1;
                     i = str.length();
                 }
             }
@@ -55,43 +60,63 @@ public class MSOE247 {
                     l += str.substring(i, i+1);
                 }
                 if (str.substring(i, i+1).equals("x")) {
-                    sp3 = i;
+                    sp3 = i+1;
                     i = str.length();
                 }
             }
             System.out.println(l);
             h = str.substring(sp3);
-            // for (int i = sp3; i < str.length(); i++) { //height FIX
-            //     if (!str.substring(i, i+1).equals("x")) {
-            //         h += str.substring(i, i+1);
-            //     }
-            //     if (str.substring(i, i+1).equals(" ")) {
-            //         i = str.length();
-            //     }
-            // }
             System.out.println(h);
             double width = Double.parseDouble(w);
-            double leng = 13.0;
+            double leng = Double.parseDouble(l);
             double height = Double.parseDouble(h);
             var g = new gift(n, width, leng, height);
+            for (int i = 0; i < list.size(); i++) {
+                if (g.getVol() > list.get(i).getVol()) {
+                    list.add(i, g);
+                }
+            }
             list.add(g);
+
+            n = "";
+            w = "";
+            l = "";
+            h = "";
+            str = input.nextLine();
         }
-        // Double.valueOf(l)
 
         list.get(0).print();
-        var g1 = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            var g2 = list.get(i);
-            if (g1.hei-g2.hei >= 1) {
-                if ((g1.wid-g2.wid >= 1 && g1.len-g2.len >= 1) || (g1.wid-g2.len >= 1 && g1.len-g2.wid >= 1)) {
-                    System.out.print(", ");
-                    g2.print();
-                }
-            } else {
-                System.out.print("\n");
-                g2.print();
-            }
-            g1 = g2;
+
+        ArrayList<ArrayList<gift>> gifts = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        var g1 = list.get(0);
+//        for (int i = 1; i < list.size(); i++) {
+//            var g2 = list.get(i);
+//            if (g1.hei-g2.hei >= 1) {
+//                if ((g1.wid-g2.wid >= 1 && g1.len-g2.len >= 1) || (g1.wid-g2.len >= 1 && g1.len-g2.wid >= 1)) {
+//                    System.out.print(", ");
+//                    g2.print();
+//                }
+//            } else {
+//                System.out.print("\n");
+//                g2.print();
+//            }
+//            g1 = g2;
+//        }
     }
 }
