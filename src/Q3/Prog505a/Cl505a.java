4d4b244ef9a7e94d30d0222242900aac94aa360d
granja.c <granja.c@stu.janesville.k12.wi.us>
2025-02-05 11:55:20 -0600

    update

--- src/Q3/Prog505a/Cl505a.java
+++ src/Q3/Prog505a/Cl505a.java
@@ -1,6 +1,8 @@
 package Q3.Prog505a;
 
-public class Cl505a {
+import Q3.Prog285b3.Cl285b3;
+
+public class Cl505a implements Comparable<Cl505a> {
     String name;
     int books;
     int points;
@@ -12,5 +14,17 @@ public class Cl505a {
         else if (books > 3 && books <= 6) points = 3 * 10 + books - 3 * 10;
         else if (books >= 6) points = 30 + (books-3) * 15;
         else points = 30 + 45 + (books-6) * 20;
+    }
+    public int compareTo(Cl505a o) {
+        return points - o.points;
+    }
+    public int getPoints() { return points; }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return name + "\t" + books + "\t" + points;
     }
 }
--- src/Q3/Prog505a/Prog505a.java
+++ src/Q3/Prog505a/Prog505a.java
@@ -1,18 +1,40 @@
 package Q3.Prog505a;
 
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import DataStructures.Set;
 
 public class Prog505a {
     public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(new File("Langdat/X.dat"));
-
+            Scanner sc = new Scanner(new File("Langdat/prog505a.dat"));
+            var readers = new Set<Cl505a>();
             while (sc.hasNext()) {
-                String str = sc.next();
-                // int
+                String fname = sc.next();
+                String lname = sc.next();
+                int books = sc.nextInt();
+                var reader = new Cl505a((fname + " " + lname), books);
+                readers.insert(reader);
             }
+            int tot = 0;
+
+            var iter = readers.iterator();
+            var arr = new ArrayList<Cl505a>();
+
+            while (iter.hasNext()) {
+                arr.add(iter.next());
+            }
+            Cl505a n = arr.get(0);
+            for (Cl505a i : arr) {
+                System.out.println(i.toString());
+                if (i.getPoints() > n.getPoints()) n = i;
+                tot += i.getPoints();
+            }
+            System.out.println("Avergae points: " + tot/arr.size());
+            System.out.println("Winner: " + n.getName());
 
         } catch (IOException e) {
             System.out.println("Can't find data file");
