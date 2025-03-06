103232c96f513c45c38c6d33b4c40edc69ff9aeb
granja-c <112727067+granja-c@users.noreply.github.com>
2025-03-05 22:00:01 -0600

    Update MSOE145.java

--- src/Q3/MSOE145.java
+++ src/Q3/MSOE145.java
@@ -8,21 +8,25 @@ public class MSOE145 {
         var facs = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
         System.out.print("Number: ");
-        int num = sc.nextInt();
-        int x = num;
+        int x = sc.nextInt();
         int fac = 2;
         while (Math.pow(fac, 2) < x) {
             if (x % fac == 0) {
                 facs.add(fac);
                 x /= fac;
-            } else fac++;
+            } else ++fac;
         }
         if (Math.pow(fac, 2) == x) {
             facs.add(fac);
-        } else fac.add(x);
+            facs.add(fac);
+        } else facs.add(x);
 
+        int f = -1;
         int res = 1;
-        for (int i:facs) res *= i;
+        for (int i:facs) {
+            if (i==f) res *= f;
+            else f = i;
+        }
         System.out.print(res);
     }
 }
