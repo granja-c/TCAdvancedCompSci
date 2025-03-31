d09d694f84deeacb109fa1a207cc415b49efc5de
granja.c <granja.c@stu.janesville.k12.wi.us>
2025-03-31 12:11:21 -0500

    update

--- src/DataStructures/PriorityQueue.java
+++ src/DataStructures/PriorityQueue.java
@@ -2,18 +2,19 @@ package DataStructures;
 
 public class PriorityQueue<T extends Comparable<T>> {
     private final Heap<T> heap;
+
     public PriotityQueue() { heap = new Heap<>(); }
-    public PriotityQueue(bool minh) { heap = new Heap<>(minh); }
+    public PriotityQueue(boolean minh) { heap = new Heap<>(minh); }
     
-    public void enqueue() { heap.insert(element); }
+    public void enqueue(T element) { heap.insert(element); }
     public T dequeue() { return heap.delete(); }
-    public T peak() { return heap.peak(); }
+    public T peek() { return heap.peek(); }
     public int size() {return heap.size(); }
-    public boolean isEmpty() { return heap.isEmpty();
+    public boolean isEmpty() { return heap.isEmpty(); }
     
-    public boolean contains(T elem) {
+    public boolean contains(T element) {
         for (int i = 0; i < heap.size(); i++) {
-            if (heap.heap[i].equals(elem)) return true;
+            if (heap.heap[i].equals(element)) return true;
         }
         return false;
     }
@@ -33,6 +34,31 @@ public class PriorityQueue<T extends Comparable<T>> {
                 heap.sink(ind);
                 heap.swim(ind);
             }
+        }
+    }
+//    public boolean contains(T element) {
+//        int ind = -1;
+//        for (int i = 0; i < heap.size(); i++) {
+//            if (heap.heap[i].equals(element)) {
+//                ind = i;
+//                break;
+//            }
+//        }
+//        return ind != -1;
+//    }
+
+    public void changePriority(T element, T newPriority) {
+        int ind = -1;
+        for (int i = 0; i < heap.size(); i++) {
+            if (heap.heap[i].equals(element)) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind != -1) {
+            heap.heap[ind] = newPriority;
+            if (heap.compare(element, newPriority) < 0) heap.sink(ind);
+            else heap.swim(ind);
         }
     }
 }
