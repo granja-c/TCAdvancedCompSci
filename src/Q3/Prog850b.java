56d8325a14b34a6284ae7fdf79ab22e71e45ae73
granja.c <granja.c@stu.janesville.k12.wi.us>
2025-04-17 12:16:29 -0500

    update

--- src/Q3/Prog850b.java
+++ src/Q3/Prog850b.java
@@ -9,17 +9,23 @@ import java.util.Scanner;
 public class Prog850b {
     public static void main(String[] args) {
         try {
-            Scanner sc = new Scanner(new File("Langdat/X.dat"));
-            Map<String, int[]> map = new HashMap<>();
+            Scanner sc = new Scanner(new File("Langdat/prog850b.txt"));
+            Map<String, Integer> map = new HashMap<>();
 
             while (sc.hasNext()) {
                 String name = sc.next();
                 int[] nums = new int[4];
-                for (int i = 0; i < 8; i++) {
+                for (int i = 0; i < 4; i++) {
                     nums[i] = sc.nextInt();
                 }
-                map.put(name, nums);
+                if (!map.containsKey(name)) { map.put(name, nums[3]); }
+                else { map.put(name, map.get(name) + nums[3]); }
             }
+
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+
 
         } catch (IOException e) {
             System.out.println("Can't find data file");
---
+++ src/Q4/Pelican553.java
@@ -0,0 +1,10 @@
+package Q4;
+
+import DataStructures.PriorityQueue;
+
+public class Pelican553 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    }
+}
